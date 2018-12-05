9a4d5a6fa387329116ad4ba69472f412e3a8b116
Kai Potter <kaipotter@MacBook-Pro.home>
2018-12-05 09:59:40 -0500

    adding tab info

--- project-final/app.R
+++ project-final/app.R
@@ -36,7 +36,7 @@ clean_choices <- c("Parabens" = "Parabens",
 # Define UI for application that draws scatterplot
 ui <- fluidPage(
    # Application title
-   titlePanel("Sephora Product Explorer"), 
+   titlePanel("Sephora Skincare Bestsellers Explorer"), 
    tabsetPanel(
      tabPanel("Explorer",
    
@@ -81,8 +81,38 @@ mainPanel(
   )
 ),
 tabPanel("About", 
-         h3("lalala"),
-            h4("lalala")),
+         h4("This Sephora Skincare Bestsellers Explorer was made to help
+you get acquainted with skincare in an accessible and informed way."),
+         h5("Creating a skincare routine from scratch can be overwhelming,
+               due to the wide variety of options. This app takes the top 30 
+            bestsellers from each product category, and shows the review 
+            statistics for each one all in one place, so you can see for
+            yourself if a product lives up to the hype, whether you really
+            need that expensive exfoliator, or if you can go with another 
+            brand that is just as good?"),
+         h5("If you're curious about a product but you're not sure if 
+it will work for you, why don't you click on the SkinCarisma.com 
+link for a  thorough ingredient breakdown? If you're acne prone, 
+you're going to want to steer clear of ingredients with a high comedogenic rating."),
+         h5("If a product claims to be anti-aging, you can check to see if the
+            ingredients actually support its claims. If it has super-star ingredients
+            such as retinoids or niacinamide for brightening, it's a go! 
+            Don't worry about not knowing exactly what these ingredients are,
+            SkinCarisma simply tells you the properties of each ingredient so
+            YOU can decide if a product is right for you, instead of letting 
+            marketing fluff tell you what is right."),
+         h5("While the skincare industry is the biggest segment in the beauty market,
+            it is also one of the most unregulated, meaning that the FDA takes a 
+            hands off approach when it comes to monitoring ingredients that go
+            into products. With the rise of hormone related health issues, 
+            such as breast cancer and poly cystic ovary syndrome, the fact 
+            that skincare can contain hormone or endocrine disruptors is disturbing, 
+            especially as it is women that are making up most of the market."),
+            h5("Sephora has recently started to give products with clean ingredients 
+a 'Clean at Sephora' seal, meaning that they are free of 'sulfates SLS and SLES,
+parabens, formaldehydes, formaldehyde-releasing agents, phthalates, mineral oil, 
+retinyl palmitate, oxybenzone, coal tar, hydroquinone, triclosan, and triclocarban.'
+You can color code ingredients with a 'Clean at Sephora' filter, or a 'Parabens' filter.")),
             
 
 tabPanel("How it works",
